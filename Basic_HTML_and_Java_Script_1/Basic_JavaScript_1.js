7b00fe1f4d26001c187f5793a9f04fc1f042abd4
hutghost <quentinshelton96@gmail.com>
2025-10-24 10:55:21 -0400

    Source Attribute Assignment

---
+++ Basic_HTML_and_Java_Script_1/Basic_JavaScript_1.js
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Basic JavaScript 1</title>
+</head>
+<html>
+<script>
+alert("Welcome to Basic JavaScript 1 by Quentin Shelton.");
+</script>
+</html>
